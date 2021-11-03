fb0db06e295422ddb17181879a78c32d7aaf0ed9
Ghislain Thiery <ghislain.thiery@ntymail.com>
2021-11-03 19:00:06 +0000

    version 1.0.0

---
+++ R/doolkit.R
@@ -0,0 +1,13 @@
+# Package dependencies:
+#' @importFrom methods new
+usethis::use_package("concaveman", type = "Imports")
+usethis::use_package("ggplot2", type = "Imports")
+usethis::use_package("igraph", type = "Imports")
+usethis::use_package("MASS", type = "Imports")
+usethis::use_package("methods", type = "Imports")
+usethis::use_package("Morpho", type = "Imports")
+usethis::use_package("rgl", type = "Imports")
+usethis::use_package("Rvcg", type = "Imports")
+usethis::use_package("sp", type = "Imports")
+usethis::use_package("tis", type = "Imports")
+usethis::use_package("usethis", type = "Imports")
